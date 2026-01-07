7cf77e4d9796225d01ee55b8f241fe3202a4c29c
AntonioMaliketh <antoniomaliketh@gmail.com>
2026-01-06 23:06:01 -0300

    osuapi handler

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-const { app, BrowserWindow, Menu, contextBridge, ipcRenderer, ipcMain } = require('electron')
+require("dotenv").config()
+
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 const path = require('node:path')
 const DefaultHTML = 'front/MainPage/index.html'
+
+if (process.env.RUN_TESTS === "true") {
+    console.log("Running tests...")
+    require("../tests/apitest.js")
+}
 
 app.whenReady().then(() => {
     Menu.setApplicationMenu(null)
@@ -19,6 +26,9 @@ app.whenReady().then(() => {
 
     ipcMain.on("window-close", () => {
         win.close()
+    })
+    ipcMain.on("window-minimize", () => {
+        win.minimize()
     })
 })
 
---
+++ tests/apitest.js
@@ -0,0 +1,2 @@
+console.log("API Test")
+require("../src/osuapi.js")
