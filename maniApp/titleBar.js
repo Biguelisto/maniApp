e41dcfc29b6699a845f5b99e3c6d77c9f6b90b4d
Felipe Tavares <felipetavaresalves123@gmail.com>
2026-09-23 23:49:25 -0300

    Match-cache

--- maniApp/titleBar.js
+++ maniApp/titleBar.js
@@ -21,15 +21,3 @@ const MinimizeButton = document.querySelector(".minimizeWindowButton")
 MinimizeButton.addEventListener("click", (e) => {
     window.titleBarAPI.Minimize()
 })
-
-setTimeout(async () => {
-    const Batch = new MatchCacheBatch()
-        .AddCall("Clear")
-        .AddCall("Write", (new MatchCache([2], "First call in the batch", "N/A")).Serialize())
-        .AddCall("Write", (new MatchCache([2], "Second call in the batch", "N/A")).Serialize())
-        .AddCall("Getfull")
-        .AddCall("Cachesize")
-        .AddCall("Read", 2)
-    const Returns = await MatchCacheBatchCall(Batch)
-    console.log(Returns[5]) // The "CacheSize"
-}, 1000)
